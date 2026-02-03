92eac9646f86a0c75de6341fac64182f143f05e2
kajalsingh748927-debug <kajalsingh748927@gmail.com>
2026-02-03 13:50:53 +0530

    URL

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,7 +3,7 @@ import mongoose  from "mongoose";
 
 const connectDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB connected");
     } catch (err) {
         console.error("MongoDB connection error:", err);
